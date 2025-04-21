1ca0888b99e41d8b685079ee3230b248b1802574
gayathri271 <gayathriratlavath27@gmail.com>
2025-04-22 01:54:18 +0530

    done

--- src/Skeleton/Skeletonloading.jsx
+++ src/Skeleton/Skeletonloading.jsx
@@ -1,12 +1,14 @@
-import Skeleton from 'react-loading-skeleton'; 
-import 'react-loading-skeleton/dist/skeleton.css'; 
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const SkeletonLoader = () => {
   return (
-    <div className="skeleton-wrapper">
-      <Skeleton height={250} width={300} /> {/* Example skeleton for image */}
-      <Skeleton count={3} /> {/* Example skeleton for text */}
-    </div>
+    <SkeletonTheme baseColor="#ddd" highlightColor="#eee">
+      <div className="skeleton-wrapper">
+        <Skeleton height={250} width={300} />
+        <Skeleton count={3} />
+      </div>
+    </SkeletonTheme>
   );
 };
 
